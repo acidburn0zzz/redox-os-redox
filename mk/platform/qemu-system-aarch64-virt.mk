efc4bc7051ab61767888475b32e6ac3b015caa15
Robin Randhawa <robin.randhawa@arm.com>
2019-01-16 13:13:38 +0000

    aarch64-prep: Add support for booting u-boot for qemu-system-aarch64-virt
    This is a mandatory requirement for this platform.

--- mk/platform/qemu-system-aarch64-virt.mk
+++ mk/platform/qemu-system-aarch64-virt.mk
@@ -1,3 +1,11 @@
+ifeq ($(UBOOT),)
+    $(error "FATAL: Platform $(PLATFORM) requires that UBOOT contain the path to a u-boot binary")
+endif
+
+ifeq ("$(wildcard $(UBOOT))","")
+    $(error "FATAL: Cannot access a u-boot binary at $(UBOOT)")
+endif
+
 QEMU_RAM_SZ=1024
 QEMU_MACHINE=virt
 PLATFORM_QEMUFLAGS=-cpu cortex-a57 -bios $(UBOOT)
