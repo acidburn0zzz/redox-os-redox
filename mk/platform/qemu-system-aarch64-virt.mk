3410bdd2aa91071e654c565e498e7818968dabc4
Robin Randhawa <robin.randhawa@arm.com>
2019-02-14 17:01:28 +0000

    qemu: Make RAM preload the default
    qemu's RAM preload makes loading the ~300MB live disk mage fast. Given
    that we don't support any "physical" storage media for AArch64 at
    present, it's ok to make this the default until we do.

--- mk/platform/qemu-system-aarch64-virt.mk
+++ mk/platform/qemu-system-aarch64-virt.mk
@@ -14,7 +14,8 @@ endif
 
 QEMU_RAM_SZ=1024
 QEMU_MACHINE=virt
-PLATFORM_QEMUFLAGS=-cpu cortex-a57 -bios $(UBOOT)
+
+PLATFORM_QEMUFLAGS+=-cpu cortex-a57 -bios $(UBOOT) -device loader,file=build/kernel.uimage,addr=0x41000000,force-raw=on
 
 KLOADADDR=0x40000000
 KENTRYADDR=0x40001000
@@ -22,10 +23,14 @@ KENTRYADDR=0x40001000
 build/livedisk.bin: build/kernel_live
 	cp $< $@
 
-post_kbuild_platform_hook:
+kernel.uimage:
 	@mkimage -A arm64 -O "linux" -T kernel -C none -a $(KLOADADDR) -e $(KENTRYADDR) -n 'Redox kernel (qemu AArch64 virt)' -d build/kernel_live build/kernel.uimage
-	$(eval UUID := $(shell cargo run --manifest-path redoxfs/Cargo.toml --release --bin redoxfs -- --get-uuid build/filesystem.bin | awk '{print $$NF}'))
+
+bootscript.ram: kernel.uimage
+	@$(eval UUID := $(shell cargo run --manifest-path redoxfs/Cargo.toml --release --bin redoxfs -- --get-uuid build/filesystem.bin | awk '{print $$NF}'))
 	@$(eval TMPFILE := $(shell mktemp))
-	@echo 'setenv bootargs REDOXFS_UUID=$(UUID) ; bootp 41000000 kernel.uimage ; bootm 41000000 - $${fdtcontroladdr}' > $(TMPFILE)
-	@mkimage -A arm64 -T script -C none -n 'Redox Bootscript' -d $(TMPFILE) build/kernel.bootscript
+	@echo 'setenv bootargs REDOXFS_UUID=$(UUID) ; bootm 41000000 - $${fdtcontroladdr}' > $(TMPFILE)
+	@mkimage -A arm64 -T script -C none -n 'Redox Bootscript' -d $(TMPFILE) build/kernel_from_ram.bootscript
 	@rm -rf $(TMPFILE)
+
+post_kbuild_platform_hook: bootscript.ram
